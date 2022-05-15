175ae9c01647018b99879c187961ba39eebeb2cd
Randy Castleman <rcastleman@gmail.com>
2022-05-15 16:52:02 -0400

    comment changed

--- returns_mc.py
+++ returns_mc.py
@@ -7,3 +7,26 @@ from numpy.random import uniform
 import matplotlib.pyplot as plt
 from datetime import date,timedelta
 from pyxirr import xirr
+
+
+# connect workbook to program
+
+#define Model and Results sheets
+
+#connect XLS static values -> program
+
+#connect XLS dynamic variable params -> program
+
+#set distributions for dynamic variables
+
+#connection XLS IRR output -> program
+
+#Simulation
+
+#collect results in dataframe
+
+#transpose inputs & outputs to be exported to worksheet
+
+#create plot(s)
+
+#Results and Plots -> XLS Results
